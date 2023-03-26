a20544853582d4af97ad84c9e601bca1b8c96724
EduRamoss3 <100438022+EduRamoss3@users.noreply.github.com>
2023-03-26 20:35:10 -0300

    Add files via upload

---
+++ Produto.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Estoque
+{
+    internal class Produto
+    {
+        public string Nome;
+        public double Preco;
+        public int Quantidade;
+
+        public void AdicionarNovoProduto(string nome, double preco, int quantidade)
+        {
+            Nome = nome;
+            Preco = preco;
+            Quantidade = quantidade;
+
+        }
+        public double ValorTotalEmEstoque()
+        {
+
+            return Preco * Quantidade;
+        }
+        public void AdicionarProdutos(int quantidade)
+        {
+            Quantidade += quantidade;
+        }
+        public void RemoverProdutos(int quantidade)
+        {
+            Quantidade -= quantidade;
+        }
+        public override string ToString()
+        {
+            return "Produto: " + Nome 
+                   +" |Preço R$|"+ Preco.ToString("F2",CultureInfo.InvariantCulture)
+                   +" |Quantidade:| "+ Quantidade
+                   +" |Valor Total em estoque:| "+ValorTotalEmEstoque().ToString("F2",CultureInfo.InvariantCulture);
+        }
+       public void MostrarOpcoes()
+        {
+           Console.WriteLine("1-MOSTRAR PRODUTOS, 2-ADICIONAR , 3-REMOVER , 4-ADICIONAR PRODUTO, 5 -SAIR");
+        }
+    }
+}
