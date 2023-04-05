c66515c979a4caa431f23cf6db7963d8c052701d
EduRamoss3 <100438022+EduRamoss3@users.noreply.github.com>
2023-04-04 22:20:24 -0300

    Introdução a vetores

---
+++ Encapsulamento/Agencia_Bancaria/Agencia_Bancaria/Pessoa.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlTypes;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Agencia_Bancaria
+{
+    internal class Pessoa
+    {
+        public int Numero { get; private set; }
+        public string Nome { get; set; }
+        public double Saldo { get; private set; }   
+        public Pessoa(int numero,string nome)
+        {
+            Numero = numero;
+            Nome = nome;
+        }
+        public Pessoa(int numero, string nome, double saldo) : this(numero, nome)
+        {
+            Saldo = saldo;
+        }
+        public override string ToString()
+        {
+            return "Conta" +
+                   Numero +
+                   ", Titular>: " +
+                   Nome +
+                   ", Saldo R$" +
+                   Saldo.ToString("F2",CultureInfo.InvariantCulture);
+        }
+    }
+
+}
---
+++ Vetor/Vetor/Estudantes.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vetor
+{
+    internal class Estudantes
+    {
+        public string Nome { get; set; }
+        public string Email { get; set; }
+
+        public Estudantes( string nome , string email )
+        {
+            Nome = nome;
+            Email = email;
+        }
+    }
+
+}
