5d93208dffe33efbd6985f15b819240e22c42a79
EduRamoss3 <100438022+EduRamoss3@users.noreply.github.com>
2023-04-06 13:25:58 -0300

    Params

---
+++ Vetor/ExercicioVetor/ExercicioVetor/Calculator.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExercicioVetor
+{
+    internal class Calculator
+    {
+        public int Numero;
+        public static int Sum( params int[] numbers)
+
+        {
+            int sum = 0;
+              for(int i = 0; i < numbers.Length; i++)
+            {
+                sum += numbers[i];
+            }
+
+            return sum;
+        }
+        public Calculator(int numero)
+        {
+            Numero = numero;
+           
+        }
+    }
+}
---
+++ Vetor/ExercicioVetor/ExercicioVetor/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExercicioVetor
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int s2 = Calculator.Sum(new int[] { 10, 20, 30, 40 });
+            int s1 = Calculator.Sum(2, 4, 3);
+
+           int[]soma1 = new int[3];
+
+            for (int i = 0; i < 3; i++)
+            {
+                Console.WriteLine("Entre com um número:");
+                int num = int.Parse(Console.ReadLine());
+                soma1[i] = num;
+
+            }
+
+            int s3 = Calculator.Sum(soma1);
+            Console.WriteLine(s3);
+            Console.Write(s2);
+            Console.Write(s1);
+
+        }
+    }
+}
