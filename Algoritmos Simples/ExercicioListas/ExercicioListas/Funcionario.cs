2a7d2658734506fcbfb7f11f10a7d4149888aedb
EduRamoss3 <100438022+EduRamoss3@users.noreply.github.com>
2023-04-18 15:55:01 -0300

    Exerc Listas

---
+++ Algoritmos Simples/ExercicioListas/ExercicioListas/Funcionario.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExercicioListas
+{
+    internal class Funcionario
+    {
+        public int? _ID { get; set; }
+        public string _Nome { get; set; }
+        public double _Salario { get; private set; }
+
+        public Funcionario(int id, string nome, double salario)
+        {
+            _ID = id;
+            _Nome = nome;
+            _Salario = salario;
+        }
+        public Funcionario()
+        {
+
+        }
+        public Funcionario(int id)
+        {
+            _ID = id;
+        }
+        public void Aumento(int id, double x)
+        {
+            _ID = id;
+            if (_ID == null)
+            {
+                Console.WriteLine("Identificador não existe");
+            }
+            else
+            {
+                x = x / 100;
+                _Salario += _Salario * x;
+            }
+
+
+        }
+        public override string ToString()
+        {
+            return +_ID + " ," + _Nome + " , " + "R$" + _Salario;
+        }
+
+    }
+}
---
+++ Algoritmos Simples/ExercicioListas/ExercicioListas/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace ExercicioListas
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            // Solução usando listas: 
+            List<Funcionario> ListaFuncionarios = new List<Funcionario>();
+            Console.WriteLine("Qual a quantidade de funcionários deseja cadastrar?");
+            int N = int.Parse(Console.ReadLine());
+             for(int i = 0; i < N; i++)
+            {
+                string nome;
+                int id;
+                double salario;
+                
+                Console.WriteLine("Cadastre o nome do "+(i+1)+".O funcionário");
+                nome = Console.ReadLine(); 
+                Console.WriteLine("Cadastre o id do funcionario: ");
+                id = int.Parse(Console.ReadLine());
+                Console.WriteLine("Cadastre o Salario do funcionário: ");
+                salario = double.Parse(Console.ReadLine());
+
+                Funcionario X = new Funcionario(id, nome, salario);
+                ListaFuncionarios.Add(X);
+
+            }
+    
+            Console.WriteLine("Digite o ID do funcionário para aumento de salário: ");
+            int identificador = int.Parse(Console.ReadLine());
+
+            foreach(Funcionario obj in ListaFuncionarios)
+            {
+                if(identificador == obj._ID)
+                {
+                    double porcentagem;
+                    Console.WriteLine("Funcionário " + identificador + obj._Nome+", Salário atual: R$"+obj._Salario);
+                    Console.WriteLine("Qual a porcentagem de aumento?");
+                    porcentagem = double.Parse(Console.ReadLine());
+                    obj.Aumento(identificador,porcentagem);
+                    Console.WriteLine("Novo salário de "+obj._Nome+" :" + obj._Salario);
+                    break;
+                }
+            }
+            foreach(Funcionario P in ListaFuncionarios)
+            {
+                Console.WriteLine(P.ToString());
+
+            }
+           
+        }
+
+    }
+}
