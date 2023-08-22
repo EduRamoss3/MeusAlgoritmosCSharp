781381e633e982a8d889771bd2c514fb9cd5fd24
Eduardo Ramos <eduardoramossilva@live.com>
2023-08-22 12:57:55 -0300

    IComparable

---
+++ Algoritmos Simples/Pathing/Path/Path/Entities/Funcionario.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+namespace Path.Entities
+{
+    internal class Funcionario : IComparable
+    {
+        public string Nome { get; set; }
+        public double Salario { get; set; }
+        public Funcionario(string csvEmployee)
+        {
+            string[] vect = csvEmployee.Split(',');
+            Nome = vect[0];
+            Salario = double.Parse(vect[1], CultureInfo.InvariantCulture);
+        }
+        public int CompareTo(object obj)
+        {
+            if(!(obj is Funcionario))
+            {
+                throw new ArgumentException("Comparing error!");
+            }
+            Funcionario funcionario = obj as Funcionario;
+
+            return Nome.CompareTo(funcionario.Nome);
+
+        }
+        public override string ToString()
+        {
+            return "Nome: " + Nome + " , Salario: " + Salario.ToString("F2",CultureInfo.InvariantCulture);
+        }
+    }
+}
---
+++ Algoritmos Simples/Pathing/Path/Path/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using Path.Entities;
+
+namespace Path
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"C:\Users\Eduardo\source\asd.txt";
+            try
+            {
+                using (StreamReader streamReader = File.OpenText(path))
+                {
+                    List<Funcionario>  list = new List<Funcionario>();
+                    while(!streamReader.EndOfStream)
+                    {
+                        list.Add(new Funcionario(streamReader.ReadLine()));
+                    }
+                    list.Sort();
+                    foreach(Funcionario func in list)
+                    {
+                        Console.WriteLine(func.ToString());
+                    }
+                }
+
+            }
+            catch (IOException E )
+            {
+                Console.WriteLine(E.Message);
+         
+            }
+            Console.ReadKey();
+        }
+    }
+}
