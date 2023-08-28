e7b19e7fece7f95d75d150f726baa225af93745a
Eduardo Ramos <eduardoramossilva@live.com>
2023-08-28 13:27:06 -0300

    Elapsed time with extension methods

---
+++ ExtensionMethods/ExtensionMethods/ExtensionMethods/Extension Methods/ExtensionTime.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExtensionMethods
+{
+    public  static class ExtensionTime
+    {
+        public static string ElapsedTime(this DateTime thisObj)
+        {
+            TimeSpan duration =  DateTime.Now.Subtract(thisObj);    
+            if(duration.TotalHours < 24)
+            {
+                return duration.TotalHours.ToString("F1",CultureInfo.InvariantCulture) + "hours";
+            }
+            else
+            {
+                return duration.TotalDays.ToString("F1",CultureInfo.InvariantCulture) + "days";
+            }
+
+         
+        }
+    }
+}
---
+++ ExtensionMethods/ExtensionMethods/ExtensionMethods/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExtensionMethods
+{
+    
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime dt = new DateTime(2018, 11, 16, 8, 10, 45);
+            Console.WriteLine("Elapsed time: "  + dt.ElapsedTime());
+            Console.ReadKey();
+        }
+    }
+}
